540851a61600263c08fdfc4f81f7d342e5cb0723
Richard Trofimov <116758171+riccorichards@users.noreply.github.com>
2024-04-24 09:40:22 +0400

    solve per day hours

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -343,10 +343,10 @@ class Service {
       ).result;
       usedTime = (totalWorkingHours / (profile.allocatedTime * 3600)) * 100;
       perDay =
-        profile.allocatedTime /
+        (profile.allocatedTime - totalWorkingHours / 3600) /
         Utils.defineRemainDays(
           profile.journeyDuration,
-          history[history.length - 1].createdAt
+          history[history.length - 1].createdAt || ""
         ).differenceInDays;
     }
 
